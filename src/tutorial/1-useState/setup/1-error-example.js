30d5af7106425c735139f54633d14ceded88f9a8
Mandi <haase1020@gmail.com>
2020-12-13 17:17:39 -0500

    finish use state tutorial

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title'
+
+
+  // no rerender and doesn't save new title
+  const handleClick = () => {
+    title = 'hello people'
+    console.log(title)
+  }
+  return (
+    <>
+  <button type="button" className="btn" onClick={handleClick}>{title}</button>
+  </>
+  );
 };
 
 export default ErrorExample;
